6d85d3564917a17eaf39cfa9ba58b2f512e2f06c
Anton <aomelyansky@gmail.com>
2022-02-09 00:07:54 +0200

    Initial commit

---
+++ app/src/main/java/edu/doggy228/antoxapos/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.doggy228.antoxapos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
